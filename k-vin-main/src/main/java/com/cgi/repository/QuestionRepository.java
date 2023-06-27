a85a135401b1f2f3c864ab59cfe3e5e50093c414
kevinmaste <79856740+kevinmaste@users.noreply.github.com>
2023-06-27 16:02:18 +0200

    Delete k-vin-main directory

--- k-vin-main/src/main/java/com/cgi/repository/QuestionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cgi.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.cgi.model.Question;
-
-public interface QuestionRepository extends MongoRepository<Question,String>  {
-
-}
